060b55735901d940e5a62c57a856525bf1ceb9f4
andreas-yiallouros <113394090+andreas-yiallouros@users.noreply.github.com>
2022-09-15 22:30:29 +0100

    Property Seles Data December

---
+++ Practice2.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This is my first line of code
+print("My name is Andreas")
+print("Today is Thursday, 15 September 2022 - it's a great day!")
+
+
+# In[2]:
+
+
+# Write a program to analyse real estate data
+
+# Sales price for each of five properties (in US$)
+A = 45000
+B = 23400
+C = 67000
+D = 34600
+E = 12900
+
+# Total sales value before commission
+total_sales_Dec = A + B + C + D + E
+
+print(total_sales_Dec)
+
+
+# In[3]:
+
+
+# Average sales value before commission
+average_sales_Dec = total_sales_Dec / 5
+
+print(average_sales_Dec)
+
+
+# In[4]:
+
+
+# Sale value after commission for property C 
+price_C = C - 6500
+
+print(price_C)
+
+
+# In[ ]:
+
+
+
+
